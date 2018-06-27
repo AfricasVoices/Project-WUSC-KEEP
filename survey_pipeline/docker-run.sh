09de9f90f7d02ae876752515d11e46281a4a4669
Alexander Simpson <as2388@cam.ac.uk>
2018-06-27 14:40:29 +0100

    Add pipeline stage for cleaning a Swahili survey.

--- survey_pipeline/docker-run.sh
+++ survey_pipeline/docker-run.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [ $# -ne 4 ]; then
-    echo "Usage: sh docker-run.sh <path_to_GitHub_key> <user> <input-file> <output-file>"
+if [ $# -ne 5 ]; then
+    echo "Usage: sh docker-run.sh <path_to_GitHub_key> <user> <input-file> <output-file> <coda-directory>"
     exit
 fi
 
@@ -11,6 +11,7 @@ GH_KEY=$1
 USER=$2
 INPUT_FILE=$3
 OUTPUT_FILE=$4
+CODA_DIR=$5
 
 # Copy key from the specified location to here, so that Docker can access it when building the image.
 cp "$GH_KEY" .gh_rsa
@@ -21,11 +22,11 @@ function finish {
 }
 trap finish EXIT
 
-# Build an image for this project, called "wusc-keep".
-docker build -t wusc-keep .
+# Build an image for this project, called "wusc-keep-survey".
+docker build -t wusc-keep-survey .
 
 # Create a container from the image that was just built.
-container="$(docker container create --env USER="$USER" wusc-keep)"
+container="$(docker container create --env USER="$USER" wusc-keep-survey)"
 
 # Copy input data into the container
 docker cp "$INPUT_FILE" "$container:/app/data/input.json"
@@ -34,4 +35,8 @@ docker cp "$INPUT_FILE" "$container:/app/data/input.json"
 docker start -a -i "$container"
 
 # Copy the output data back out of the container
+mkdir -p "$(dirname "$OUTPUT_FILE")"
 docker cp "$container:/app/data/output.json" "$OUTPUT_FILE"
+
+mkdir -p "$CODA_DIR"
+docker cp "$container:/app/data/coda/." "$CODA_DIR"
